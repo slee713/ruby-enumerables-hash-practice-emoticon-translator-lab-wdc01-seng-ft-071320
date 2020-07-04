892864c31bc7c911041f6ae101dbc5270c8c8d45
Steven Lee <steven.lee1393@gmail.com>
2020-07-04 16:31:40 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,56 @@
 # require modules here
-
-def load_library
+require "yaml"
+require "pry"
+def load_library(emoticons)
   # code goes here
+  library = YAML.load_file(emoticons)
+  # binding.pry
+  library.each_with_object({}) do |(expression, emotes), organized_lib|
+    organized_lib[expression]={}
+    emotes.each do |emote|
+      if emote == emotes[0]
+        organized_lib[expression][:english] = emote
+      elsif emote == emotes[1]
+        organized_lib[expression][:japanese] = emote
+      end
+    end
+  end
+
+end
+# binding.pry
+def get_japanese_emoticon(source, emoticon)
+  # code goes here
+  library = load_library(source)
+  # binding.pry
+
+  library.find do |expression, languages|
+  # binding.pry
+    values = languages.collect {|k, v| v}
+    # binding.pry
+    values.include? emoticon
+        # japanese_emote = languages[:japanese]
+      # binding.pry
+    end
+
+  #  binding.pry
 end
 
-def get_japanese_emoticon
+def get_english_meaning(source, emoticon)
   # code goes here
-end
+  library = load_library(source)
+  # binding.pry
+  library.each do |expression, languages|
 
-def get_english_meaning
-  # code goes here
+    values = languages.collect {|k, v| v}
+    # # binding.pry
+      if values.include? emoticon
+          return expression
+      end
+      if emoticon == nil
+        return "Sorry, that emoticon was not found"
+      end
+    end
+
+
+
 end
