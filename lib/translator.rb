4f152389922d2aa36f12d9aa4177ba83131e758d
Steven Lee <steven.lee1393@gmail.com>
2020-07-05 18:25:19 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,56 +1,52 @@
 # require modules here
+
 require "yaml"
 require "pry"
+
+
+
 def load_library(emoticons)
   # code goes here
   library = YAML.load_file(emoticons)
+  sorted_lib = {}
   # binding.pry
-  library.each_with_object({}) do |(expression, emotes), organized_lib|
-    organized_lib[expression]={}
-    emotes.each do |emote|
-      if emote == emotes[0]
-        organized_lib[expression][:english] = emote
-      elsif emote == emotes[1]
-        organized_lib[expression][:japanese] = emote
-      end
-    end
+  library.each do |expression, emote|
+    sorted_lib[expression] ={}
+    sorted_lib[expression][:english]=emote[0]
+    sorted_lib[expression][:japanese]=emote[1]
   end
-
+  sorted_lib
+  #binding.pry
 end
 # binding.pry
 def get_japanese_emoticon(source, emoticon)
   # code goes here
   library = load_library(source)
   # binding.pry
-
-  library.find do |expression, languages|
+  emote = library.keys.find do |key|
+    library[key][:english] == emoticon
+  end
   # binding.pry
-    values = languages.collect {|k, v| v}
-    # binding.pry
-    values.include? emoticon
-        # japanese_emote = languages[:japanese]
-      # binding.pry
-    end
-
-  #  binding.pry
+  emote ? library[emote][:japanese]: "Sorry, that emoticon was not found"
 end
+
 
 def get_english_meaning(source, emoticon)
   # code goes here
+
+
   library = load_library(source)
+  expression = library.keys.find do |key|
+    library[key][:japanese] == emoticon
+  end
+  expression ? expression: "Sorry, that emoticon was not found"
   # binding.pry
-  library.each do |expression, languages|
-
-    values = languages.collect {|k, v| v}
-    # # binding.pry
-      if values.include? emoticon
-          return expression
-      end
-      if emoticon == nil
-        return "Sorry, that emoticon was not found"
-      end
-    end
-
-
-
+#   library.each do |expression, languages|
+#     values = languages.collect {|k, v| v}
+#     # binding.pry
+#       if values.include? emoticon
+#         return expression
+#       end
+#     end
+#     return "Sorry, that emoticon was not found"
 end
