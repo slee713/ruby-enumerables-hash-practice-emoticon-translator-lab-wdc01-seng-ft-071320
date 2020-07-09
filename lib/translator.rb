57aaa7acdff5aa345d3a4cc503ec8cc252b8e7da
Steven Lee <steven.lee1393@gmail.com>
2020-07-09 20:24:45 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,37 +16,30 @@ def load_library(emoticons)
     sorted_lib[expression][:japanese]=emote[1]
   end
   sorted_lib
-  #binding.pry
 end
-# binding.pry
+
+
 def get_japanese_emoticon(source, emoticon)
   # code goes here
   library = load_library(source)
-  # binding.pry
-  emote = library.keys.find do |key|
-    library[key][:english] == emoticon
-  end
-  # binding.pry
-  emote ? library[emote][:japanese]: "Sorry, that emoticon was not found"
+  japan_emote = "Sorry, that emoticon was not found"
+  library.each do |expression, emoticons|
+    if emoticons[:english] == emoticon
+      japan_emote = emoticons[:japanese]
+    end
+end
+   japan_emote
 end
 
 
-def get_english_meaning(source, emoticon)
-  # code goes here
 
+
+
+def get_english_meaning(source, emoticon)
 
   library = load_library(source)
   expression = library.keys.find do |key|
     library[key][:japanese] == emoticon
   end
   expression ? expression: "Sorry, that emoticon was not found"
-  # binding.pry
-#   library.each do |expression, languages|
-#     values = languages.collect {|k, v| v}
-#     # binding.pry
-#       if values.include? emoticon
-#         return expression
-#       end
-#     end
-#     return "Sorry, that emoticon was not found"
 end
